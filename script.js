9261b2b098985ea01031e99dfb86141e9e2a4e5d
Davi Medeiros <daviabmedeiros@gmail.com>
2024-01-09 14:13:34 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+  function toggleMode(){
+    const html = document.documentElement
+    //toggle, uma função já existente de "botão switch"
+    html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if(html.classList.contains('light')){
+    //mudando atributo da img de acordo com o fundo
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Mayk Brito de óculos')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+  
+
+}
