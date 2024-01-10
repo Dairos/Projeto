0e1214f523b42a12f4c77304c283eae04c0481de
Davi Medeiros <daviabmedeiros@gmail.com>
2024-01-10 19:15:29 -0300

    Adicionei meu jogo e canal no youtube

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-  function toggleMode(){
-    const html = document.documentElement
-    //toggle, uma função já existente de "botão switch"
-    html.classList.toggle("light")
+function toggleMode() {
+  const html = document.documentElement
+  //toggle, uma função já existente de "botão switch"
+  html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
-  if(html.classList.contains('light')){
+  if (html.classList.contains("light")) {
     //mudando atributo da img de acordo com o fundo
-    img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute('alt', 'Mayk Brito de óculos')
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Mayk Brito de óculos")
   } else {
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute("src", "./assets/perfil-dairos.png")
+    
   }
-
-  
-
 }
